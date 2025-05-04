5304dd477c28a66ddb1812b4a3fb03ee502a29bd
hooriya-11 <hooriashakeelmughal@gmail.com>
2025-05-04 21:59:54 +0500

    to do list

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+//})
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// const port = 5002;
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true })); // To handle form data
+
+let tasks = []; // In-memory task list
+
+// Show ToDo list page
+app.get('/', (req, res) => {
+res.render('todo', { tasks: tasks });
+});
+
+// Add task
+app.post('/add', (req, res) => {
+const task = req.body.task;
+if (task) tasks.push(task);
+res.redirect('/');
+});
+
+// Server start
+app.listen(port, () => {
+console.log ('Server is running on http://localhost:${port}');
+});
